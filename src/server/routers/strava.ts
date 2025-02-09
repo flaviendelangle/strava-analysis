50344e8ae2c53de71f8bc8be4dacbbac8d8a0abe
flavien <flaviendelangle@gmail.com>
2025-02-09 10:44:39 +0100

    FIx

--- src/server/routers/strava.ts
+++ src/server/routers/strava.ts
@@ -1,7 +1,3 @@
-/**
- *
- * This is an example router, you can delete this file and then update `../pages/api/trpc/[trpc].tsx`
- */
 import { router, publicProcedure } from '../trpc';
 
 export const postRouter = router({
